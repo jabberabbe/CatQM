cfe781925882e4def80ef0e4638ecda9634737f5
Tito Sacchi <tito.sakki@gmail.com>
2020-02-29 10:40:19 +0100

    Dependently-typed BigOps with mathcomp's finType library
    (The current implementation cannot be easily used with computation
    techniques. I would like to reimplement this with another implementation of
    finTypes)

--- BigOps.v
+++ BigOps.v
@@ -1,7 +1,8 @@
 Set Warnings "-notation-overridden".
 
+Require Import Program.
 From Coq Require Import ssreflect.
-From mathcomp.ssreflect Require Import ssrnat ssrbool bigop.
+From mathcomp.ssreflect Require Import ssrnat ssrbool bigop fintype eqtype.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -21,45 +22,66 @@ Section BigOps.
 Context {C : Category}.
 Context `{_ : @Monoidal C} `{S' : @Superposition C} `{Z : @Zero C} `{_ : @Biproduct C S' Z}.
 
-Fixpoint biprodN `{_ : @Monoidal C} (X : obj[C]) (n : nat) : obj[C] :=
+Lemma ordinal_equality {k a : nat} (p q : ltn a (S k)) : (Ordinal p) = (Ordinal q).
+Proof. by apply/val_inj. Defined.
+
+Lemma ord0_fin1 (k : ordinal 1) : k = ord0.
+Proof.
+  destruct k; unfold ord0.
+  have Q : m = 0%N by move: m i; case.
+  move: i; rewrite Q.
+  symmetry; by apply ordinal_equality.
+Qed.
+
+Definition ord_widen_succ (a : nat) (b : ordinal a) : ordinal (S a).
+Proof. case: b => m i; construct. exact (S m). exact i. Defined.
+
+Program Fixpoint biprodN' (n : nat) (X : ordinal (n.+1) -> obj[C]) : obj[C] :=
     match n with
-    | O => X
-    | S n' => X ⨁ (biprodN X n')
+    | O => (X ord0)
+    | S n' => (X ord0) ⨁ (@biprodN' n' (fun k => X (ord_widen_succ k)))
     end.
 
-Lemma biprodN_succ {X : obj[C]} {n : nat} : (X ⨁ (biprodN X n))%object = (biprodN X (S n)).
+Definition biprodN (n : nat) (X : obj[C]) := @biprodN' n (fun _ => X).
+
+Lemma biprodN_succ {X : obj[C]} {n : nat} : (X ⨁ (biprodN n X))%object = (biprodN (S n) X).
 Proof. reflexivity. Qed.
 
-Lemma biprod0 {X : obj[C]} : X = biprodN X 0.
+Lemma biprod0 {X : obj[C]} : X = biprodN 0 X.
 Proof. reflexivity. Qed.
 
-Fixpoint inj_n  {X : obj[C]} (maxn : nat) (n : nat) : X ~> biprodN X maxn :=
-    match maxn with
-    | O => id
-    | S maxn' => 
-        match n with
-        | O => inj_l
-        | S n' => inj_r ∘ (inj_n maxn' n')
-        end
-    end.
+Lemma biprod0' {X : ordinal 1 -> obj[C]} : (X ord0) = biprodN' (n:=0) X.
+Proof. reflexivity. Qed.
 
-Fixpoint proj_n {X : obj[C]} (maxn : nat) (n : nat) : biprodN X maxn ~> X :=
-    match maxn with
-    | O => id
-    | S maxn' =>
-        match n with
-        | O => proj_l
-        | S n' => (proj_n maxn' n') ∘ proj_r
-        end
-    end.
+Fixpoint inj_n' (maxn : nat) {X : ordinal (maxn.+1) -> obj[C]} (n : ordinal (maxn.+1)) : (X n) ~> biprodN' (n:=maxn) X.
+Proof.
+    move: X n.
+    case: maxn => [X n //=|maxn X].
+    - case: n => [m H']; rewrite (ord0_fin1 (Ordinal H')); exact id.
+    - case; case => [i //=|n i]. 
+      + rewrite /ord0 (ordinal_equality i (ltn0Sn (S maxn))); exact inj_l.
+      + simpl; have P : (ltn n (S maxn)) by [].
+        have -> : (Ordinal i) = (ord_widen_succ (Ordinal P)) by rewrite /ord_widen_succ ordinal_equality.
+        exact (inj_r ∘ (@inj_n' maxn (fun k => X (ord_widen_succ k)) (Ordinal P))).
+Defined.
+Definition inj_n {maxn : nat} {X : C} (n : ordinal (maxn.+1)%N) : X ~> biprodN maxn X := @inj_n' maxn (fun _ => X) n.
+
+Fixpoint proj_n' (maxn : nat) {X : ordinal (maxn.+1) -> obj[C]} (n : ordinal (maxn.+1)) : biprodN' (n:=maxn) X ~> (X n).
+Proof.
+    move: X n.
+    case: maxn => [X n //=|maxn X].
+    - case: n => [m H']; rewrite (ord0_fin1 (Ordinal H')); exact id.
+    - case; case => [i //=|n i]. 
+      + rewrite /ord0 (ordinal_equality i (ltn0Sn (S maxn))); exact proj_l.
+      + simpl; have P : (ltn n (S maxn)) by [].
+        have -> : (Ordinal i) = (ord_widen_succ (Ordinal P)) by rewrite /ord_widen_succ ordinal_equality.
+        exact ((@proj_n' maxn (fun k => X (ord_widen_succ k)) (Ordinal P)) ∘ proj_r).
+Defined.
+Definition proj_n {maxn : nat} {X} := @proj_n' maxn (fun _ => X).
+
 Ltac done := cat_simpl.
 
-Lemma inj_n_succ {X : obj[C]} (maxn n : nat) : (@inj_n X (maxn.+1) (n.+1)) ≈ inj_r ∘ (inj_n maxn n).
-Proof. reflexivity. Qed.
-Lemma proj_n_succ {X: obj[C]} (maxn n : nat) : (@proj_n X (maxn.+1) (n.+1)) ≈ (proj_n maxn n) ∘ proj_r.
-Proof. reflexivity. Qed.
-
-Definition bigsum {A B : obj[C]} (f : nat -> A ~> B) (maxn : nat) : A ~> biprodN B maxn :=
-    \big [sum/unit]_ (0 <= i < maxn) ((inj_n maxn i) ∘ (f i)).
+Definition bigsum {A : obj[C]} {B : nat -> obj[C]} (f : forall n : nat, A ~> (B n)) (maxn : nat) : A ~> @biprodN' maxn B :=
+  \big [sum/unit]_ (i in (ordinal maxn.+1%N)) ((inj_n' i) ∘ (f i)).
 
 End BigOps.
